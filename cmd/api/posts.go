2fc07aa155a15eebb30de940fbbcc925b578c707
rohinish404 <singhrohinish09@gmail.com>
2025-10-27 13:45:19 +0530

    add comments, update and delete post

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,6 +9,9 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/rohinish404/social-go/internal/store"
 )
+
+type postKey string
+const postCtx postKey = "post"
 
 type CreatePostPayload struct {
 	Title   string   `json:"title" validate:"required,max=100"`
@@ -49,6 +53,24 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	post := getPostFromCtx(r)
+
+
+	comments, err := app.store.Comments.GetByPostID(r.Context(), post.ID)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return 
+	}
+
+	post.Comments = comments
+
+	if err := WriteJSON(w, http.StatusCreated, post); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) deletePostHandler(w http.ResponseWriter, r *http.Request){
 	idParam := chi.URLParam(r, "postID")
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
@@ -57,8 +79,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	ctx := r.Context()
 
-	post, err := app.store.Posts.GetByID(ctx, id)
-	if err != nil {
+	if err := app.store.Posts.Delete(ctx, id); err != nil {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
 			app.notFoundResponse(w, r, err)
@@ -67,8 +88,72 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	if err := WriteJSON(w, http.StatusCreated, post); err != nil {
+	
+	w.WriteHeader(http.StatusNoContent)
+}
+type UpdatePostPayload struct {
+	Title *string `json:"title" validate:"omitempty,max=100"`
+	Content *string `json:"content" validate:"omitempty,max=1000"`
+
+}
+func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request){
+	  post := getPostFromCtx(r)
+
+	  var payload UpdatePostPayload
+
+	  if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return 
+	  }
+
+	  if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	  }
+
+	  if payload.Content != nil {
+		post.Content = *payload.Content
+	  }
+	  if payload.Title != nil {
+		post.Content = *payload.Title
+	  }
+
+	  if err:= app.store.Posts.Update(r.Context(), post); err != nil {
 		app.internalServerError(w, r, err)
 		return
-	}
+	  }
+
+	  if err := WriteJSON(w, http.StatusOK, post); err != nil {
+		app.internalServerError(w, r, err)
+	  }
+}
+
+func (app *application) postsContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "postID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.internalServerError(w, r, err)
+			return
+		}
+		ctx := r.Context()
+
+		post, err := app.store.Posts.GetByID(ctx, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.notFoundResponse(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+		ctx = context.WithValue(ctx, postCtx, post)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getPostFromCtx(r *http.Request) *store.Post {
+	post, _ := r.Context().Value(postCtx).(*store.Post)
+	return post
 }
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -16,6 +16,7 @@ type Post struct {
 	Tags      []string `json:"tags"`
 	CreatedAt string   `json:"created_at"`
 	UpdatedAt string   `json:"updated_at"`
+	Comments []Comment `json:"comments"`
 }
 
 type PostStore struct {
@@ -61,4 +62,38 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		}
 	}
 	return &post, nil
+}
+
+func (s *PostStore) Delete(ctx context.Context, postID int64) error {
+	query := `DELETE FROM posts WHERE id = $1`
+
+	res, err := s.db.ExecContext(ctx, query,postID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+}
+
+func (s *PostStore) Update(ctx context.Context, post *Post) error {
+	query := `
+	UPDATE posts
+	SET title = $1, content = $2
+	WHERE id = $3
+	`
+
+	_, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
